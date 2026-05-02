2fe6f2498f9a61aa68b044b070fbaf3a0df1babf
KhazGPT <225504039+welkhazen@users.noreply.github.com>
2026-05-02 15:05:26 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,6 +25,7 @@ const SignupModalLazy = lazy(() =>
 
 const Index = () => {
   const [showMatrixIntro, setShowMatrixIntro] = useState(true);
+
   const {
     user,
     isLoggedIn,
@@ -167,28 +168,3 @@ const Index = () => {
           onSignupClick={() => setShowSignup(true)}
         />
         <AvatarShowcaseSection />
-        <WheelReward
-          onSignupClick={() => setShowSignup(true)}
-        />
-        <EarnedWarUpgradesSection />
-        <AnonQuestionSection />
-        <WhyAnonymity />
-      </div>
-      <LandingFooter />
-
-      <Suspense fallback={null}>
-        {showSignup ? (
-          <SignupModalLazy
-            open={showSignup}
-            onClose={() => setShowSignup(false)}
-            onRequestSignupOtp={requestSignupOtp}
-            onVerifySignupOtp={verifySignupOtp}
-            onLogin={login}
-          />
-        ) : null}
-      </Suspense>
-    </div>
-  );
-};
-
-export default Index;
